41a599b37bccf65350b999f6e698fce259f984bc
Juliano Sill <sill.juliano@gmail.com>
2024-08-28 23:55:24 -0300

    feat(routes): create index route base

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify'
 import { errorHandler } from 'http/error-handler'
+import { routes } from 'http/routes'
 
 const app = fastify()
 
 app.setErrorHandler(errorHandler)
+app.register(routes)
 
 app
   .listen({
